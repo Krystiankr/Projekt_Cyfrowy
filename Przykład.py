c7c7c65b31e60533c9b1d77666209ef640d8a703
Krystian <162847@student.uwm.edu.pl>
2021-12-21 13:20:24 +0100

    Wprowadzenie hermetyzacji funkcji i dodana przykładowa klasa z użyciem metod.

--- Przykład.py
+++ Przykład.py
@@ -2,7 +2,7 @@ from Create_Table import CreateTable
 
 
 if __name__ == "__main__":
-    # table.return.df() -> pierwsze columny to reprezentacja lizcby binarnej, druga to Y, trzecia to count(1) liczba jedynek, tylko gdy Y == 1, inaczej Y == -1
+    # table.return.df() -> pierwsze columny to reprezentacja liczby binarnej, druga od konca to Y, ostatnia to count(1) liczba jedynek, tylko gdy Y == 1, inaczej Y == -1
     # table.first_group -> zwraca typ defaultdict, klucze to grupy, values: np. [-1, 0, 0, 1] gdzie -1 to 'x'
     # table.print_first_group() -> wypisuje w ładny sposob poszczególne grupy i ich elementy
     # wprowadzone jest ograniczenie liczb to max 40
